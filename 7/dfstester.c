b623707a64c223fd8505f0310ed74fc7e1791c8d
dynamite-123 <anirudhkashyap182005@gmail.com>
2025-07-22 19:44:26 +0530

    connectivity

--- 7/dfstester.c
+++ 7/dfstester.c
@@ -3,10 +3,11 @@
 
 
 
-int graph[100][100], visited[100], isCyclic = 0;
+int graph[100][100], visited[100], isCyclic = 0, dfscount = 0;
 
 void dfs(int n, int node) {
     printf("--> %c ", node + 65);
+    dfscount++;
 
     visited[node] = 1; //visiting 
     for (int i = 0; i < n; i++) {
@@ -41,7 +42,12 @@ int main() {
         visited[i] = 0;
     }
 
-    for (int i=0; i<n; i++) {
+    dfs(n, 0);
+    printf("\n");
+
+    int connected = (n == dfscount)? 1: 0;
+
+    for (int i=1; i<n; i++) {
         if (visited[i] == 0) {
             dfs(n, i);
         }
@@ -52,6 +58,12 @@ int main() {
         printf("Graph is cyclic\n");
     } else {
         printf("Graph is acyclic\n");
+    }
+
+    if (connected == 1) {
+        printf("Graph is connected\n");
+    } else {
+        printf("Graph is not connected\n");
     }
     return 0;
 }
